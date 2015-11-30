68bc5749f8d759c6377d18c8c599f39cd8bce10b
wusan <406964280@qq.com>
2015-11-30 11:33:02 +0800

    Create read.php

---
+++ read.php
@@ -0,0 +1,11 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<p>php文件。。</p>
+<p>php文件5566。。</p>
+<?php
+	//数组操作	
+	$arr1=array('This','car','is','red');
+	print_r($arr1)."<p>";//Array ( [0] => This [1] => car [2] => is [3] => red )
+	echo $arr1[1]."<p>";//取某个元素
+	$arr1[4]="Like";
+	echo $arr1[4]."<p>";//取某个元素
+?>
