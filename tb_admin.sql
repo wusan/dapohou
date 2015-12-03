e48898d9939db6e445faf8b378bbe752d95b1730
wusan <406964280@qq.com>
2015-12-03 10:33:25 +0800

    Create tb_admin.sql

---
+++ tb_admin.sql
@@ -0,0 +1,25 @@
+
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+-- 
+-- 数据库: `db_study`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- 表的结构 `tb_admin100`
+-- 
+
+CREATE TABLE `tb_admin100` (
+  `id` varchar(3) NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 
+-- 导出表中的数据 `tb_admin100`
+-- 
+
+INSERT INTO `tb_admin100` VALUES ('122', '中4国');
+INSERT INTO `tb_admin100` VALUES ('623', '人3民');
